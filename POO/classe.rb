6afa03d3725a759ed843b9599d6146159b029c72
Ademar Neto <ademarnetodev@gmail.com>
2024-11-12 23:32:13 -0300

    POO

---
+++ POO/classe.rb
@@ -0,0 +1,5 @@
+#definido classe:
+#A calsse é a base para um objetos
+#Exemplo eu quero contruir um prédio (objeto), para isso eu preciso de uma planta (class)
+#A classe é uma descrição de como será o objeto
+#sozinha ele é apenas im desenho, mas junto com o objeto começamos a ves algo prático
