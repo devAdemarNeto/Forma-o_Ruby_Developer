e82eb12d6af73a5fa88af558f16467008bc81354
Ademar Neto <ademarnetodev@gmail.com>
2024-11-13 22:27:32 -0300

    POO

--- POO/herança.rb
+++ POO/herança.rb
@@ -1,2 +1,13 @@
 #herança
-#Para herdar caractrísticas de outra classe, adicione na frente do nome de uma classe filha o símbolo de menor e depois a classe pai
+#Para herdar caractrísticas de outra classe, adicione na frente do nome de uma classe filha o símbolo de menor e depois a classe pai.
+
+#ex animal 
+
+
+class Animal
+    def dormir
+        'zzzz'
+    end
+
+    def pular
+        
